88ad0bd3daf22ca9534161ddaa294fe43b91934f
wochowskaj83 <wochowskaj@gmail.com>
2023-06-13 09:27:06 +0200

    rozwiazanie zadania

--- js/script.js
+++ js/script.js
@@ -1,56 +1,21 @@
-console.log(`Warsztat zad45976`);
+console.log(`Warsztat zad45974`);
 
 
-/* Wariant I */
-/* $(document).ready(function() {
-
-    $('#get-data').click(function() {
-        $.get('https://akademia108.pl/api/ajax/get-post.php').done(function(data) {
-
-            let pId = $('<p></p>').text(`Post ID: ${data.id}`);
-            let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
-            let pTitle = $('<p></p>').text(`Title: ${data.title}`);
-            let pBody = $('<p></p>').text(`Body: ${data.body}`);
-            let hr = $('<hr />');
-
-            let jqBody = $('body');
-
-            jqBody.append(pId);
-            jqBody.append(pUserId);
-            jqBody.append(pTitle);
-            jqBody.append(pBody);
-            jqBody.append(hr);
-
-
-        })
-            .fail(function(error) {
-                console.log(error);
-            });
-    });
-}); */
-
-
-
-/* Wariant II */
 $(document).ready(function() {
 
     $('#get-data').click(function() {
-        $.getJSON('https://akademia108.pl/api/ajax/get-post.php').done(function(data) {
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php').done(function(data) {
 
-            let pId = $('<p></p>').text(`Post ID: ${data.id}`);
-            let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
-            let pTitle = $('<p></p>').text(`Title: ${data.title}`);
-            let pBody = $('<p></p>').text(`Body: ${data.body}`);
+            let dUserId = $('<div id="dane-programisty"></div>').text(`Autor: ${data.imie} ${data.nazwisko} ${data.zawod} ${data.firma}`);
+
             let hr = $('<hr />');
 
             let jqBody = $('body');
 
-            jqBody.append(pId);
-            jqBody.append(pUserId);
-            jqBody.append(pTitle);
-            jqBody.append(pBody);
-            jqBody.append(hr);
 
+            jqBody.append(dUserId);
+
+            jqBody.append(hr);
 
         })
             .fail(function(error) {
